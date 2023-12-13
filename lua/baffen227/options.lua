35bda70467ad46f4b863f761263f1c771852bb52
baffen227 <baffen227@gmail.com>
2023-12-13 15:31:18 +0800

    Add options for autocomplete

--- lua/baffen227/options.lua
+++ lua/baffen227/options.lua
@@ -5,3 +5,9 @@ vim.opt.cursorline=true               -- Show cursor line
 vim.opt.number=true                   -- Show line numbers
 vim.opt.wrap=false                    -- Do not wrap text
 
+-- Autocomplete
+vim.opt.complete="."                  -- Only use current buffer for autocomplete
+-- Show menu even with one entry only, do not select nor insert, and show preview
+vim.opt.completeopt="menuone,noselect,noinsert,preview"
+vim.opt.pumheight=10                  -- Menu height (number of lines)
+
