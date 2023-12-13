c20a5cac6752bee7a2baa0490c3f34a7d43b6792
baffen227 <baffen227@gmail.com>
2023-12-13 15:08:38 +0800

    Add options about appearance

---
+++ lua/baffen227/options.lua
@@ -0,0 +1,7 @@
+
+-- Appearance
+vim.opt.shortmess="I"                 -- Remove splash screen
+vim.opt.cursorline=true               -- Show cursor line
+vim.opt.number=true                   -- Show line numbers
+vim.opt.wrap=false                    -- Do not wrap text
+
