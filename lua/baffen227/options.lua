52cde4f5250e62012dc3fd66c265ee58a0f6cbc4
baffen227 <baffen227@gmail.com>
2023-12-23 11:40:49 +0800

    Enable relativenumber option by default

--- lua/baffen227/options.lua
+++ lua/baffen227/options.lua
@@ -2,6 +2,7 @@
 vim.opt.shortmess="I"                 -- Remove splash screen
 vim.opt.cursorline=true               -- Show cursor line
 vim.opt.number=true                   -- Show line numbers
+vim.opt.relativenumber=true           -- Show relative numbers
 vim.opt.wrap=false                    -- Do not wrap text
 
 -- Autocomplete
